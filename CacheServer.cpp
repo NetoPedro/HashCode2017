93537f4d8820a91b2f15d8249b4f97758388b135
NetoPedro <pedroneto_09@hotmail.com>
2018-01-19 19:46:45 +0000

    CacheServer class

---
+++ CacheServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pedro Neto on 15/01/18.
+//
+
+#include "CacheServer.h"
---
+++ CacheServer.h
@@ -0,0 +1,15 @@
+//
+// Created by Pedro Neto on 15/01/18.
+//
+
+#ifndef HASHCODE2017_CACHESERVER_H
+#define HASHCODE2017_CACHESERVER_H
+
+
+class CacheServer {
+public: int id ;
+    double maxCapacity ;
+};
+
+
+#endif //HASHCODE2017_CACHESERVER_H
